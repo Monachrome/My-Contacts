c887b936433b6a17b8a90f231ba6d6dfd171014a
Monachrome <monalisa.banerjee8991@gmail.com>
2022-08-24 00:03:07 +0530

    Updates

---
+++ precache-manifest.c78e2c4c7308e5a4ef1457e32cbecf36.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e4f8f482257bc14fc2c06c3be478813",
+    "url": "/My-Contacts/index.html"
+  },
+  {
+    "revision": "bc816f983a6680963541",
+    "url": "/My-Contacts/static/js/2.09df5970.chunk.js"
+  },
+  {
+    "revision": "8fdbf1416493c91170da",
+    "url": "/My-Contacts/static/js/main.8cc4446f.chunk.js"
+  },
+  {
+    "revision": "8e8caeb83c8d1591e4d1",
+    "url": "/My-Contacts/static/js/runtime-main.9104d340.js"
+  }
+]);
